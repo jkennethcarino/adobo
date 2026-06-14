054bf756dca9b0a00f9883f6208028084b9ee3fb
Jhon Kenneth Cariño <6307355+jkennethcarino@users.noreply.github.com>
2026-06-14 15:24:11 +0800

    fix(Reddit - Disable home feed swipe): Support `2026.14.0` and `2026.04.0`

--- patches/src/main/kotlin/dev/jkcarino/adobo/patches/reddit/layout/homeswipe/DisableHomeSwipePatch.kt
+++ patches/src/main/kotlin/dev/jkcarino/adobo/patches/reddit/layout/homeswipe/DisableHomeSwipePatch.kt
@@ -2,6 +2,7 @@ package dev.jkcarino.adobo.patches.reddit.layout.homeswipe
 
 import app.morphe.patcher.extensions.InstructionExtensions.addInstruction
 import app.morphe.patcher.extensions.InstructionExtensions.getInstruction
+import app.morphe.patcher.extensions.InstructionExtensions.removeInstruction
 import app.morphe.patcher.patch.bytecodePatch
 import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 import dev.jkcarino.adobo.patches.reddit.misc.firebase.spoofCertificateHashPatch
@@ -10,7 +11,8 @@ import dev.jkcarino.adobo.patches.reddit.shared.COMPATIBILITY_REDDIT
 @Suppress("unused")
 val disableHomeSwipePatch = bytecodePatch(
     name = "Disable home feed swipe",
-    description = "Disables the horizontal page swipe gesture used to switch feeds."
+    description = "Disables the horizontal page swipe gesture used to switch feeds.",
+    default = false
 ) {
     compatibleWith(COMPATIBILITY_REDDIT)
 
@@ -28,6 +30,16 @@ val disableHomeSwipePatch = bytecodePatch(
                 index = 0,
                 smaliInstructions = "const/16 v$userScrollEnabledRegister, 0x0"
             )
+        }
+
+        SwipingVariantFingerprint.method.apply {
+            val isVariantEnabledIndex =
+                SwipingVariantFingerprint.instructionMatches.first().index
+            val variantIndex =
+                SwipingVariantFingerprint.instructionMatches[4].index
+
+            removeInstruction(variantIndex)
+            removeInstruction(isVariantEnabledIndex)
         }
     }
 }
