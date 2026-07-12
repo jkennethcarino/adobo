3279d00b84ac1bda537046a763b63ccd904010d3
Jhon Kenneth Cariño <6307355+jkennethcarino@users.noreply.github.com>
2026-07-12 21:59:01 +0800

    refactor(Gboard - Toggle feature flags): Rework and support other boolean flag variant

--- patches/src/main/kotlin/dev/jkcarino/adobo/patches/google/gboard/misc/ocr/EnableOcrPatch.kt
+++ patches/src/main/kotlin/dev/jkcarino/adobo/patches/google/gboard/misc/ocr/EnableOcrPatch.kt
@@ -1,27 +1,27 @@
 package dev.jkcarino.adobo.patches.google.gboard.misc.ocr
 
-import app.morphe.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.morphe.patcher.patch.bytecodePatch
 import dev.jkcarino.adobo.patches.google.gboard.detection.signature.bypassSignaturePatch
+import dev.jkcarino.adobo.patches.google.gboard.featureflags.toggleFeatureFlag
 import dev.jkcarino.adobo.patches.google.gboard.shared.COMPATIBILITY_GBOARD
 
 @Suppress("unused")
 val enableOcrPatch = bytecodePatch(
     name = "Enable OCR feature",
-    description = "Enables OCR feature to extract text from images and insert it into text fields."
+    description = "Enables the Scan Text feature to extract text from images " +
+        "and insert them into text fields."
 ) {
     compatibleWith(COMPATIBILITY_GBOARD)
 
     dependsOn(bypassSignaturePatch)
 
     execute {
-        OcrAccessPointFingerprint.method.apply {
-            val isEnabledIndex = OcrAccessPointFingerprint.instructionMatches.last().index
-
-            replaceInstruction(
-                index = isEnabledIndex,
-                smaliInstruction = "const/4 v1, 0x1"
-            )
-        }
+        // Sources:
+        //   - Nail Sadykov (X/Twitter: @Nail_Sadykov)
+        //   - GMS Flags: https://github.com/polodarb/GMS-Flags
+        toggleFeatureFlag(
+            flag = "enable_ocr",
+            enabled = true
+        )
     }
 }
